45a312a053a78d5d3d0a2754cee03205975b7f02
Muser Quantity <muserquantity@gmail.com>
2022-07-06 14:43:04 +0800

    add name of timeutil

--- example/timeutil_test.go
+++ example/timeutil_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestTimestamp(t *testing.T) {
+func TestTimeUtil(t *testing.T) {
 	timestamp, err := timeutil.ParseTimeStrInLocation(timeutil.PureNumLayout, "20220706142622", timeutil.ShanghaiTimeZone)
 	if err != nil {
 		t.Fatal(err)
